5da0b9fb86371648eb6c6abc4dac7124102ba350
Miyang <3468605499@qq.com>
2021-11-19 22:56:11 +0800

    第五次作业

---
+++ Source/TPSAnimation/TPSAnimationGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSAnimationGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATPSAnimationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATPSAnimationGameMode();
+};
+
+
+
